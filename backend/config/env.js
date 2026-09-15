bf5fd26ba84745842f8c5b608a1a0d5d87104649
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-09-15 18:05:30 +0100

    configure last update

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 3000,
-  N8N_HOST: process.env.N8N_HOST,
+  N8N_WEBHOOK_URL: process.env.N8N_WEBHOOK_URL,
 };
 // configure the project //
