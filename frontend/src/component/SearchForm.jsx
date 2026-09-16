f0db0197545ee4ab1bbd1d00376a785b31a3b99e
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-09-16 18:29:25 +0100

    add Search Form component

--- frontend/src/component/SearchForm.jsx
+++ frontend/src/component/SearchForm.jsx
@@ -1,19 +1,33 @@
-export default function SearchForm() {
-  <div className="search-form">
-    <input
-      type="text"
-      placeholder="Search Query (ex: Ecole privée)"
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-    />
-    <input
-      type="text"
-      placeholder="Localisation (ex: Casablanca, Morocco)"
-      value={location}
-      onChange={(e) => setLocation(e.target.value)}
-    />
-    <button onClick={onScrape} disabled={loading}>
-      {loading ? "Scraping en cours..." : "Scraper"}
-    </button>
-  </div>;
+export default function SearchForm({
+  searchQuery,
+  setSearchQuery,
+  location,
+  setLocation,
+  onScrape,
+  loading,
+}) {
+  return (
+    <div className="search-form">
+      <label htmlFor="search-query">Sujet de recherche</label>
+      <input
+        id="search-query"
+        type="text"
+        placeholder="(ex: Ecole privée ,Ain Chock)"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
+      <label htmlFor="location">Localisation</label>
+      <input
+        id="location"
+        type="text"
+        placeholder="Localisation (ex: Casablanca, Morocco)"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button onClick={onScrape} disabled={loading}>
+        {loading ? "Scraping en cours..." : "Scraper"}
+      </button>
+    </div>
+  );
 }
